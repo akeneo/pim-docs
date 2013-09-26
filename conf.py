9a0022705221478c46f67ee894246c6defceb8f4
Filips Alpe <filips@akeneo.com>
2013-09-26 11:33:06 +0200

    Create akeneo theme

--- conf.py
+++ conf.py
@@ -102,29 +102,15 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'akeneo'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-    'collapsiblesidebar': 'true',
-    'footerbgcolor':      '#b183cb',
-    'sidebarbgcolor':     '#f2f2f2',
-    'sidebarbtncolor':    '#9665b1',
-    'sidebartextcolor':   '#484848',
-    'sidebarlinkcolor':   '#8d45b6',
-    'relbarbgcolor':      '#8d45b6',
-    'textcolor':          '#484848',
-    'linkcolor':          '#9665b1',
-    'visitedlinkcolor':   '#9665b1',
-    'headtextcolor':      '#484848',
-    'headlinkcolor':      '#9665b1',
-    'codebgcolor':        '#eee'
-}
+# html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = ['_themes']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
