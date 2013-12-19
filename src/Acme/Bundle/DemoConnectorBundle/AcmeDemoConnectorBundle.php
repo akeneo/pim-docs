0a91ebee656622872dbb23a322cb8c390a02f11d
Nicolas Dupont <nicolas@akeneo.com>
2013-12-19 09:57:05 +0100

    PIM-1657 : add demo connector bundle in doc

---
+++ src/Acme/Bundle/DemoConnectorBundle/AcmeDemoConnectorBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Acme\Bundle\DemoConnectorBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Oro\Bundle\BatchBundle\Connector\Connector;
+
+class AcmeDemoConnectorBundle extends Connector
+{
+}
