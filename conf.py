e4a49afd71c8b569e7a1a0c29ad0aab83ce0d173
Julien Breux <julien.breux@gmail.com>
2014-11-10 19:28:33 +0100

    Fix copyright year

--- conf.py
+++ conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Akeneo PIM'
-copyright = u'2013, Akeneo SAS'
+copyright = u'2014, Akeneo SAS'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
