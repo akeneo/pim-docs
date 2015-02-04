f0ea5745fd8159c9836ac7e5767044791b328660
Olivier Soulet <olivier.soulet@akeneo.com>
2015-02-04 17:59:44 +0100

    Fix logo for new theme

--- generate.sh
+++ generate.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 echo "Removing existing files within ../pim-docs-build/*"
-rm -rf ../pim-docs-build/*;
+rm ../pim-docs-build/* -rf;
 echo "Generting docs in ../pim-docs-build/*"
 sphinx-build -b html . ../pim-docs-build/;
-echo "Copying images."
-cp _images/logo.png ../pim-docs-build/_images/
-echo "Done."
-
