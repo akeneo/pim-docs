4bfc49862954b1ab088dfd3a4d63ed973eac6c3e
Nicolas Dupont <nicolas@akeneo.com>
2015-02-06 16:19:58 +0100

    Fix theme

--- generate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "Removing existing files within ../pim-docs-build/*"
-rm ../pim-docs-build/* -rf;
-echo "Generting docs in ../pim-docs-build/*"
-sphinx-build -b html . ../pim-docs-build/;
