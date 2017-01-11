b5e2dc344d0209a516158f7591a3b99332d59b9f
Pierre Allard <pierallard@gmail.com>
2017-01-11 10:06:25 +0100

    Update composer.json to allow error

--- styleguide/index.php
+++ styleguide/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once("vendor/autoload.php");
+require_once("../vendor/autoload.php");
 
 $loader = new \Twig_Loader_Filesystem(__DIR__.'/templates');
 $twig = new \Twig_Environment($loader);
