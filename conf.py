831534504515789006f561798b38ef35077ca79c
Pierre Allard <pierallard@gmail.com>
2017-09-30 15:14:42 +0200

    [test-v2] Test new design

--- conf.py
+++ conf.py
@@ -117,7 +117,7 @@ keep_warnings = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'akeneo_rtd'
+html_theme = 'akeneo'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
